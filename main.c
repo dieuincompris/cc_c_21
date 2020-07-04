4301064560cc2196ecda71650e67c4ca1732ff0c
dieuincompris <grandmouginalban@gmail.com>
2020-07-04 09:19:11 +0000

    1 tour de jeu

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 #include "game.h"
 
+int p_hand = 0;
+
 int main()
 {
     t_game * g = game_init();
@@ -12,4 +14,22 @@ int main()
 
 void game_loop(t_game * g)
 {
+print_player_coins(g->player);
+broker_new_hand(g);
+player_new_hand(g);
+print_player_hand(g->player);
+broker_place_bet(g);
+player_place_bet(g); 
+sum_player_hand(g->player);
+print_turn_results(g, p_hand);
+
+if (player_breakthrough(g))
+{
+  player_lost(g);
+}
+
+if (player_jackpot(g))
+{
+  player_win(g);
+}
 }
